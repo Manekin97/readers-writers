29eda2303e013311c752253e459ecd39c12d03f9
Manekin97 <rafikdab@gmail.com>
2018-05-16 21:37:10 +0200

    Fixed readers and writers starvation.

--- src/WritersStarvation.h
+++ src/WritersStarvation.h
@@ -8,18 +8,8 @@
 #include <unistd.h>
 #include <utime.h>
 
-// queueState_t* state;
-
-// sem_t readTry;
-// pthread_mutex_t wmutex, rmutex;
 pthread_mutex_t m;
 pthread_cond_t writersQ, readersQ;
-
-// int readersInside;
-// int writersInside;
-// int readersQ;
-// int writersQ;
-
 int readers, writers;
 
 
